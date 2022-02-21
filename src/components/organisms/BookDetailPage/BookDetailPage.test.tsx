8b4b793cb3a8be39aa56b8af420339e307f7ac7d
iamkiran27 <saikiran.kavali@zemosolabs.com>
2022-02-21 09:00:58 +0530

    testing-10

--- src/components/organisms/BookDetailPage/BookDetailPage.test.tsx
+++ src/components/organisms/BookDetailPage/BookDetailPage.test.tsx
@@ -1,29 +1,69 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import {
+  findByAltText,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import BookDetailPage from "./BookDetailPage";
 import "@testing-library/jest-dom";
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 
-test("Render SearchIcon Button", () => {
+// test("Render SearchIcon Button", () => {
+//   render(
+//     <MemoryRouter>
+//       <BookDetailPage
+//         booksdata={[
+//           {
+//             id: 5,
+//             image: "https://www.linkpicture.com/q/book6.png",
+//             name: "Loving Your Business",
+//             author: "Debbie King",
+//             time: "13-minute read",
+//             finished: false,
+//             added: false,
+//           },
+//         ]}
+//         handleFinish={() => {
+//           console.log("Handle finish triggerd");
+//         }}
+//       />
+//     </MemoryRouter>
+//   );
+//   expect(screen.getByTestId("book-detail-page")).toBeInTheDocument();
+// });
+
+test("my Component test", async () => {
+  const id = "5";
+
   render(
-    <MemoryRouter>
-      <BookDetailPage
-        booksdata={[
-          {
-            id: 5,
-            image: "https://www.linkpicture.com/q/book6.png",
-            name: "Loving Your Business",
-            author: "Debbie King",
-            time: "13-minute read",
-            finished: false,
-            added: false,
-          },
-        ]}
-        handleFinish={() => {
-          console.log("Handle finish triggerd");
-        }}
-      />
+    <MemoryRouter initialEntries={[`/book/${id}`]}>
+      <Routes>
+        <Route
+          path="/book/:id"
+          element={
+            <BookDetailPage
+              booksdata={[
+                {
+                  id: 5,
+                  image: "https://www.linkpicture.com/q/book6.png",
+                  name: "Loving Your Business",
+                  author: "Debbie King",
+                  time: "13-minute read",
+                  finished: false,
+                  added: false,
+                },
+              ]}
+              handleFinish={() => {
+                console.log("Handle finish triggerd");
+              }}
+            />
+          }
+        />
+      </Routes>
     </MemoryRouter>
   );
-  expect(screen.getByTestId("book-detail-page")).toBeInTheDocument();
+
+  await screen.findByTestId("book-detail-page");
 });
